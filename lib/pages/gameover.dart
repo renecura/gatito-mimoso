11a404f4c89aaf734131f6c68662e693aa97752a
Rodrigo Rene Cura <rodrigo.renecura@gmail.com>
2023-12-08 11:59:37 -0300

    Pantallitas, casi listo

--- lib/pages/gameover.dart
+++ lib/pages/gameover.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GameOver extends StatelessWidget {
+class GameOver extends ConsumerWidget {
   const GameOver({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Volver'),
-          onPressed: () => Navigator.pop(context),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SizedBox(
+                height: 256,
+                child: Image.asset('assets/images/gatito/gatito07.png')),
+            ElevatedButton(
+              child: const Text('Volver'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ]),
         ),
       ),
     );
